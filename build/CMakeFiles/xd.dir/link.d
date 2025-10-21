ca7c33eb623c8b222576abb0f0cadc0d51e23030
NotAidan <notaidan99@gmail.com>
2025-10-21 01:51:10 -0700

    Critical Fix: Fixed how the lexer processes key words. Also fixed Generator.cpp to work better with llvm

---
+++ build/CMakeFiles/xd.dir/link.d
@@ -0,0 +1,277 @@
+xd: \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/Scrt1.o \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/crti.o \
+  /usr/lib/gcc/x86_64-linux-gnu/14/crtbeginS.o \
+  CMakeFiles/xd.dir/src/main.cpp.o \
+  CMakeFiles/xd.dir/src/lexer.cpp.o \
+  CMakeFiles/xd.dir/src/parser.cpp.o \
+  CMakeFiles/xd.dir/src/generator.cpp.o \
+  /usr/local/lib/libLLVMCore.a \
+  /usr/local/lib/libLLVMSupport.a \
+  /usr/local/lib/libLLVMIRReader.a \
+  /usr/local/lib/libLLVMExecutionEngine.a \
+  /usr/local/lib/libLLVMMC.a \
+  /usr/local/lib/libLLVMMCJIT.a \
+  /usr/local/lib/libLLVMX86CodeGen.a \
+  /usr/local/lib/libLLVMX86AsmParser.a \
+  /usr/local/lib/libLLVMX86Desc.a \
+  /usr/local/lib/libLLVMX86Disassembler.a \
+  /usr/local/lib/libLLVMX86Info.a \
+  /usr/local/lib/libLLVMExecutionEngine.a \
+  /usr/local/lib/libLLVMOrcTargetProcess.a \
+  /usr/local/lib/libLLVMOrcShared.a \
+  /usr/local/lib/libLLVMRuntimeDyld.a \
+  /usr/local/lib/libLLVMAsmPrinter.a \
+  /usr/local/lib/libLLVMCFGuard.a \
+  /usr/local/lib/libLLVMGlobalISel.a \
+  /usr/local/lib/libLLVMIRPrinter.a \
+  /usr/local/lib/libLLVMInstrumentation.a \
+  /usr/local/lib/libLLVMSelectionDAG.a \
+  /usr/local/lib/libLLVMCodeGen.a \
+  /usr/local/lib/libLLVMTarget.a \
+  /usr/local/lib/libLLVMObjCARCOpts.a \
+  /usr/local/lib/libLLVMScalarOpts.a \
+  /usr/local/lib/libLLVMAggressiveInstCombine.a \
+  /usr/local/lib/libLLVMInstCombine.a \
+  /usr/local/lib/libLLVMCGData.a \
+  /usr/local/lib/libLLVMBitWriter.a \
+  /usr/local/lib/libLLVMTransformUtils.a \
+  /usr/local/lib/libLLVMAnalysis.a \
+  /usr/local/lib/libLLVMProfileData.a \
+  /usr/local/lib/libLLVMSymbolize.a \
+  /usr/local/lib/libLLVMDebugInfoGSYM.a \
+  /usr/local/lib/libLLVMDebugInfoDWARF.a \
+  /usr/local/lib/libLLVMDebugInfoDWARFLowLevel.a \
+  /usr/local/lib/libLLVMDebugInfoPDB.a \
+  /usr/local/lib/libLLVMObject.a \
+  /usr/local/lib/libLLVMIRReader.a \
+  /usr/local/lib/libLLVMAsmParser.a \
+  /usr/local/lib/libLLVMBitReader.a \
+  /usr/local/lib/libLLVMTextAPI.a \
+  /usr/local/lib/libLLVMDebugInfoCodeView.a \
+  /usr/local/lib/libLLVMDebugInfoMSF.a \
+  /usr/local/lib/libLLVMDebugInfoBTF.a \
+  /usr/local/lib/libLLVMFrontendHLSL.a \
+  /usr/local/lib/libLLVMCore.a \
+  /usr/local/lib/libLLVMRemarks.a \
+  /usr/local/lib/libLLVMBitstreamReader.a \
+  /usr/local/lib/libLLVMCodeGenTypes.a \
+  /usr/local/lib/libLLVMMCParser.a \
+  /usr/local/lib/libLLVMMCDisassembler.a \
+  /usr/local/lib/libLLVMMC.a \
+  /usr/local/lib/libLLVMBinaryFormat.a \
+  /usr/local/lib/libLLVMTargetParser.a \
+  /usr/local/lib/libLLVMSupport.a \
+  /usr/local/lib/libLLVMDemangle.a \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/librt.a \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libdl.a \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libstdc++.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libm.so \
+  /lib/x86_64-linux-gnu/libm.so.6 \
+  /lib/x86_64-linux-gnu/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libc.so \
+  /lib/x86_64-linux-gnu/libc.so.6 \
+  /usr/lib/x86_64-linux-gnu/libc_nonshared.a \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/14/crtendS.o \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/crtn.o
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/Scrt1.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/crti.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/crtbeginS.o:
+
+CMakeFiles/xd.dir/src/main.cpp.o:
+
+CMakeFiles/xd.dir/src/lexer.cpp.o:
+
+CMakeFiles/xd.dir/src/parser.cpp.o:
+
+CMakeFiles/xd.dir/src/generator.cpp.o:
+
+/usr/local/lib/libLLVMCore.a:
+
+/usr/local/lib/libLLVMSupport.a:
+
+/usr/local/lib/libLLVMIRReader.a:
+
+/usr/local/lib/libLLVMExecutionEngine.a:
+
+/usr/local/lib/libLLVMMC.a:
+
+/usr/local/lib/libLLVMMCJIT.a:
+
+/usr/local/lib/libLLVMX86CodeGen.a:
+
+/usr/local/lib/libLLVMX86AsmParser.a:
+
+/usr/local/lib/libLLVMX86Desc.a:
+
+/usr/local/lib/libLLVMX86Disassembler.a:
+
+/usr/local/lib/libLLVMX86Info.a:
+
+/usr/local/lib/libLLVMExecutionEngine.a:
+
+/usr/local/lib/libLLVMOrcTargetProcess.a:
+
+/usr/local/lib/libLLVMOrcShared.a:
+
+/usr/local/lib/libLLVMRuntimeDyld.a:
+
+/usr/local/lib/libLLVMAsmPrinter.a:
+
+/usr/local/lib/libLLVMCFGuard.a:
+
+/usr/local/lib/libLLVMGlobalISel.a:
+
+/usr/local/lib/libLLVMIRPrinter.a:
+
+/usr/local/lib/libLLVMInstrumentation.a:
+
+/usr/local/lib/libLLVMSelectionDAG.a:
+
+/usr/local/lib/libLLVMCodeGen.a:
+
+/usr/local/lib/libLLVMTarget.a:
+
+/usr/local/lib/libLLVMObjCARCOpts.a:
+
+/usr/local/lib/libLLVMScalarOpts.a:
+
+/usr/local/lib/libLLVMAggressiveInstCombine.a:
+
+/usr/local/lib/libLLVMInstCombine.a:
+
+/usr/local/lib/libLLVMCGData.a:
+
+/usr/local/lib/libLLVMBitWriter.a:
+
+/usr/local/lib/libLLVMTransformUtils.a:
+
+/usr/local/lib/libLLVMAnalysis.a:
+
+/usr/local/lib/libLLVMProfileData.a:
+
+/usr/local/lib/libLLVMSymbolize.a:
+
+/usr/local/lib/libLLVMDebugInfoGSYM.a:
+
+/usr/local/lib/libLLVMDebugInfoDWARF.a:
+
+/usr/local/lib/libLLVMDebugInfoDWARFLowLevel.a:
+
+/usr/local/lib/libLLVMDebugInfoPDB.a:
+
+/usr/local/lib/libLLVMObject.a:
+
+/usr/local/lib/libLLVMIRReader.a:
+
+/usr/local/lib/libLLVMAsmParser.a:
+
+/usr/local/lib/libLLVMBitReader.a:
+
+/usr/local/lib/libLLVMTextAPI.a:
+
+/usr/local/lib/libLLVMDebugInfoCodeView.a:
+
+/usr/local/lib/libLLVMDebugInfoMSF.a:
+
+/usr/local/lib/libLLVMDebugInfoBTF.a:
+
+/usr/local/lib/libLLVMFrontendHLSL.a:
+
+/usr/local/lib/libLLVMCore.a:
+
+/usr/local/lib/libLLVMRemarks.a:
+
+/usr/local/lib/libLLVMBitstreamReader.a:
+
+/usr/local/lib/libLLVMCodeGenTypes.a:
+
+/usr/local/lib/libLLVMMCParser.a:
+
+/usr/local/lib/libLLVMMCDisassembler.a:
+
+/usr/local/lib/libLLVMMC.a:
+
+/usr/local/lib/libLLVMBinaryFormat.a:
+
+/usr/local/lib/libLLVMTargetParser.a:
+
+/usr/local/lib/libLLVMSupport.a:
+
+/usr/local/lib/libLLVMDemangle.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/librt.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libdl.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libstdc++.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libm.so:
+
+/lib/x86_64-linux-gnu/libm.so.6:
+
+/lib/x86_64-linux-gnu/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libc.so:
+
+/lib/x86_64-linux-gnu/libc.so.6:
+
+/usr/lib/x86_64-linux-gnu/libc_nonshared.a:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/crtendS.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/crtn.o:
